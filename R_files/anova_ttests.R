4df3663f32a0dc5dc4984f56388bf26af16433ae
codes-witch <daniverratti@gmail.com>
2023-12-19 23:33:49 +0100

    post-hoc t-tests done

--- R_files/anova_ttests.R
+++ R_files/anova_ttests.R
@@ -24,6 +24,7 @@ anova_results <- list()
 unique_feats <- unique(normalized_students$feature)
 
 non_significant_feats <- c()
+significant_feats <- c()
 nan_results_feats <- c()
 zero_feats_all_learners <- c()
 
@@ -50,6 +51,8 @@ for (constr in unique_feats) {
       nan_results_feats <- append(nan_results_feats, constr)
     } else if (p_value > 0.05) {
       non_significant_feats <- append(non_significant_feats, constr)
+    } else {
+      significant_feats <- append(significant_feats, constr)
     }
   }
   
@@ -60,38 +63,63 @@ normalized_students <- add_feature_level(normalized_students)
 
 # For all significant features,  do T-tests to check for significant differences between the feature level and the previous one. 
 
-# All A2 features: are they they significantly different in A2 texts from A1 texts?
+levels <- c("A1", "A2", "B1", "B2", "C1", "C2")
 
-significant_a2_feats <- unique(filter(normalized_students, feat_level == "A2", !feature %in% non_significant_feats, !feature %in% zero_feats_all_learners)$feature)
-no_signif_diff_a1_a2<- c()
-signif_difference_a1_a2 <- c()
-no_occurrences_a1_a2 <- c()
+t_tests_p_values <- data.frame(matrix(ncol = 6, nrow = length(significant_feats)))
+names(t_tests_p_values) <- c("feature", "A1_A2", "A2_B1", "B1_B2", "B2_C1", "C1_C2")
 
 
-levels <- c("C2", "C1", "B2", "B1", "A2", "A1")
 
-for (constr in significant_a2_feats) {
-  
-  if (sum(filter(normalized_students, text_level %in% c("A1", "A2"), feature == constr)$total) == 0) {
-    print(paste0("Contsruct ", constr, " does not appear in any texts written by A1 or A2 students"))
-    no_occurrences_a1_a2 <- append(no_occurrences_a1_a2, constr)
-  } else {
-  
-  t_test_result <- normalized_students %>%
-    filter(feature == constr, text_level %in% c("A1", "A2")) %>%
-    t.test(total ~ text_level, data = .)
-
-  
-    if (t_test_result$p.value > 0.05) {
-      no_signif_diff_a1_a2<- append(no_difference_a1_a2, constr)
-      print(paste(constr, " has no significant difference between A1 and A2"))
-    } else {
-      signif_difference_a1_a2 <- append(signif_difference_a1_a2, constr)
+# For all relevant constructs, do a t-test between all consecutive levels
+for (f in 1:length(significant_feats)) {
+    constr <- significant_feats[f]
+    p_values <- c()
+    for (i in 1:(length(levels) - 1)){
+      level1 <- levels[i]
+      level2 <- levels[i + 1]
+      print(paste0("Construct: ", constr))
+      print(paste0("Level 1: ", level1))
+      print(paste0("Level 2: ", level2))
+      
+      # Check that the current feature appears in the level before or in the current level
+      if (sum(filter(normalized_students, text_level %in% c(level1, level2), feature == constr)$total) == 0) { 
+        print(paste0("Contsruct ", constr, " does not appear in any texts written by ", level1, " or ",  level2, " students"))
+      }
+      
+      t_test_result <- normalized_students %>%
+        filter(feature == constr, text_level %in% c(level1, level2)) %>%
+        t.test(total ~ text_level, data = .)
+      
+      p_values <- append(p_values, t_test_result$p.value)
+     
     }
-  }
-  
+    
+    construct_pval_row <- append(as.character(constr), p_values)
+    #names(construct_pval_row) <- names(t_tests_p_values)
+    print(construct_pval_row)
+    t_tests_p_values[f,] <- construct_pval_row
+    
 }
 
 
 
-  
+t_tests_p_values <- add_feature_level(t_tests_p_values)
+t_tests_p_values <- rename_column(t_tests_p_values, 7, "EGP_level")
+
+t_tests_p_values <- rename_column(t_tests_p_values, 1, "construct_ID")
+
+
+a2_mismatch <- t_tests_p_values %>%
+  filter(EGP_level == "A2", as.numeric(A1_A2) > 0.05)
+
+b1_mismatch <-  t_tests_p_values %>%
+  filter(EGP_level == "B1", as.numeric(A2_B1) > 0.05)
+
+b2_mismatch <- t_tests_p_values %>%
+  filter(EGP_level == "B2", as.numeric(B1_B2) > 0.05)
+
+c1_mismatch <- t_tests_p_values %>%
+  filter(EGP_level == "C1", as.numeric(B2_C1) > 0.05)
+
+c2_mismatch <- t_tests_p_values %>%
+  filter(EGP_level == "C2", as.numeric(C1_C2) > 0.05)
