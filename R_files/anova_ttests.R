4a6e80a4fe8a6cc7c634a187ec9c8a2f55c1db9d
codes-witch <daniverratti@gmail.com>
2024-03-06 20:56:37 +0100

    renamed things to anova* and t_test*

--- R_files/anova_ttests.R
+++ R_files/anova_ttests.R
@@ -1,3 +1,4 @@
+# For each student of each level, how many times they used each feature per thousand words
 a1_normalized_counts_students_long$text_level = "A1"
 a2_normalized_counts_students_long$text_level = "A2"
 b1_normalized_counts_students_long$text_level = "B1"
@@ -24,9 +25,9 @@ anova_results <- list()
 
 unique_feats <- unique(normalized_students$feature)
 
-non_significant_feats <- c()
-significant_feats <- c()
-nan_results_feats <- c()
+anova_non_sign_constructs <- c()
+anova_sign_constructs <- c()
+anova_nan_results_constructs <- c()
 zero_feats_all_learners <- c()
 
 # ANOVA. Check which features have significant differences between their frequencies in levels levels.
@@ -48,12 +49,12 @@ for (constr in unique_feats) {
     
     p_value = anova_results[[constr]][[1]][[5]][1]
   
-    if (is.nan(p_value)){ # nan_results_feats will remain empty because we are discarding all features with no occurrences
-      nan_results_feats <- append(nan_results_feats, constr)
+    if (is.nan(p_value)){ # sanity check: anova_nan_results_constructs will remain empty because we are discarding all features with no occurrences
+      anova_nan_results_constructs <- append(anova_nan_results_constructs, constr)
     } else if (p_value > 0.05) {
-      non_significant_feats <- append(non_significant_feats, constr)
+      anova_non_sign_constructs <- append(anova_non_sign_constructs, constr)
     } else {
-      significant_feats <- append(significant_feats, constr)
+      anova_sign_constructs <- append(anova_sign_constructs, constr)
     }
   }
   
@@ -66,22 +67,25 @@ normalized_students <- add_feature_level(normalized_students)
 
 levels <- c("A1", "A2", "B1", "B2", "C1", "C2")
 
-t_tests_p_values <- data.frame(matrix(ncol = 6, nrow = length(significant_feats)))
+# store p-values from t-tests
+t_tests_p_values <- data.frame(matrix(ncol = 6, nrow = length(anova_sign_constructs)))
 names(t_tests_p_values) <- c("feature", "A1_A2", "A2_B1", "B1_B2", "B2_C1", "C1_C2")
+
+# store whether the higher level has significantly higher frequency
+t_tests_l2_greater <- data.frame(matrix(ncol = 6, nrow = length(anova_sign_constructs)))
+names(t_tests_l2_greater) <- c("feature", "A1_A2", "A2_B1", "B1_B2", "B2_C1", "C1_C2")
 
 
 
 # For all relevant constructs, do a t-test between all consecutive levels
-# TODO significance matters in terms of which one is higher!! You are missing that info by only taking into account the p-value. 
-for (f in 1:length(significant_feats)) {
-    constr <- significant_feats[f]
+for (f in 1:length(anova_sign_constructs)) {
+    constr <- anova_sign_constructs[f]
     p_values <- c()
+    l2_greater <- c() # Whether the T test estimated a greater mean for the greater level
     for (i in 1:(length(levels) - 1)){
       level1 <- levels[i]
       level2 <- levels[i + 1]
       print(paste0("Construct: ", constr))
-      #print(paste0("Level 1: ", level1))
-      #print(paste0("Level 2: ", level2))
       
       appears_in_lev1 <- sum(filter(normalized_students, text_level == level1, feature == constr)$total) > 0
       appears_in_lev2 <- sum(filter(normalized_students, text_level == level2, feature == constr)$total) > 0
@@ -97,122 +101,97 @@ for (f in 1:length(significant_feats)) {
         }
         
         p_values <- append(p_values, NA) 
-      } else { # It must appear in BOTH levels being compared
-      
-      t_test_result <- normalized_students %>%
-        filter(feature == constr, text_level %in% c(level1, level2)) %>%
-        t.test(total ~ text_level, data = .)
-      
-      p_values <- append(p_values, t_test_result$p.value)
+        l2_greater <- append(l2_greater, NA)
+      } else { # It must appear in BOTH levels to be compared
+        
+        t_test_result <- normalized_students %>%
+          filter(feature == constr, text_level %in% c(level1, level2)) %>%
+          t.test(total ~ text_level, data = .)
+        
+        p_values <- append(p_values, t_test_result$p.value)
+        if (t_test_result$p.value <= 0.05){
+          l2_greater <- append(l2_greater, t_test_result$statistic < 0)
+          
+          
+        } else {
+          l2_greater <- append(l2_greater, NA)
+        }
+        
       }
      
     }
-    
+    print(l2_greater)
     construct_pval_row <- append(as.character(constr), p_values)
+    construct_l2_greater_row <- append(as.character(constr), l2_greater)
     #names(construct_pval_row) <- names(t_tests_p_values)
     print(construct_pval_row)
     t_tests_p_values[f,] <- construct_pval_row
+    t_tests_l2_greater[f, ] <- construct_l2_greater_row
     
 }
 
+# We only care about construct-level pairs that have TRUE in t_tests_l2_greater, but we care about their p_value
+t_tests_relevant_p_vals <- data.frame(matrix(ncol = 6, nrow = length(anova_sign_constructs)))
+names(t_tests_relevant_p_vals) <- c("feature", "A1_A2", "A2_B1", "B1_B2", "B2_C1", "C1_C2")
 
+t_tests_relevant_p_vals$feature <- t_tests_p_values$feature
 
-t_tests_p_values <- add_feature_level(t_tests_p_values)
-t_tests_p_values <- rename_column(t_tests_p_values, 7, "EGP_level")
-
-t_tests_p_values <- rename_column(t_tests_p_values, 1, "construct_ID")
-
-
-a2_mismatch <- t_tests_p_values %>%
-  filter(EGP_level == "A2", as.numeric(A1_A2) > 0.05)
-
-b1_mismatch <-  t_tests_p_values %>%
-  filter(EGP_level == "B1", as.numeric(A2_B1) > 0.05)
-
-b2_mismatch <- t_tests_p_values %>%
-  filter(EGP_level == "B2", as.numeric(B1_B2) > 0.05)
-
-c1_mismatch <- t_tests_p_values %>%
-  filter(EGP_level == "C1", as.numeric(B2_C1) > 0.05)
-
-c2_mismatch <- t_tests_p_values %>%
-  filter(EGP_level == "C2", as.numeric(C1_C2) > 0.05)
-
-
-# for each construct, get the mean at each level and make a line plot
-library(ggplot2)
-
-#for (constr in significant_feats){
-normalized_counts_students_long %>%
-  group_by(feature, text_level) %>%
-  filter(feature == 1111) %>%
-  summarise(mean = mean(total)) %>%
-    ggplot(aes(x = text_level, y = mean, color = as.factor(feature), group = feature)) +
-    geom_line() +
-    xlab("Levels") +
-    ylab("Mean frequency of construct") +
-    scale_x_discrete(
-      breaks = c("A1", "A2", "B1", "B2", "C1", "C2"),
-      labels = c("A1", "A2", "B1", "B2", "C1", "C2")
-    ) +
-    ggtitle("Typical C1 construct")
-#}
-
-#for (f in c(148, 475, 740, 1057)) {
-normalized_counts_students_long %>%
-  filter(feature == 1057) %>%
-  group_by(learnerID, text_level) %>%
-  ggplot(aes(x = text_level, y = total, fill = text_level)) +
-  geom_boxplot() +
-  xlab("Text Level") +
-  ylab("Mean of Total") +
-  ggtitle(paste0("Boxplot for feature ", 1057))
-
-for (col in 2:6) {
-  t_tests_p_values[, col] <- as.numeric(t_tests_p_values[,col])
+# populate t_tests_relevant_p_vals with p values, only when they are significant and the second level is of higher frequency than the first one
+for (col_idx in 2:ncol(t_tests_relevant_p_vals)) { # the first column has the construct ID, skip
+  for (row_idx in 1:nrow(t_tests_relevant_p_vals)) {
+    if (!is.na(t_tests_l2_greater[row_idx, col_idx]) && as.logical(t_tests_l2_greater[row_idx, col_idx])) {
+      t_tests_relevant_p_vals[row_idx, col_idx] <- t_tests_p_values[row_idx, col_idx]
+    }
+  }
 }
 
-level_prediction <- data.frame(feature = significant_feats) %>%
+
+t_tests_relevant_p_vals <- add_feature_level(t_tests_relevant_p_vals)
+t_tests_relevant_p_vals <- rename_column(t_tests_relevant_p_vals, 7, "EGP_level")
+
+t_tests_relevant_p_vals <- rename_column(t_tests_relevant_p_vals, 1, "construct_ID")
+
+
+for (col in 2:6) {
+  t_tests_relevant_p_vals[, col] <- as.numeric(t_tests_relevant_p_vals[,col])
+}
+
+t_level_predictions <- data.frame(feature = anova_sign_constructs) %>%
   add_feature_level() %>%
   rename(EGP_level = feat_level) %>%
   rename(construct = feature) %>%
   mutate(min_p_pred = NA, first_sign_p_pred = NA, signif_at_EGP_level = NA)
 
-row.names(level_prediction) <- significant_feats
 
 # Find first significant diff
-for (r in 1:nrow(t_tests_p_values)){
+for (r in 1:nrow(t_tests_relevant_p_vals)){
+  print(t_tests_relevant_p_vals[r, 1])
   for (c in 2:6){
-    #print(paste0("Row ", r, " Column ", c))
-    #print(t_tests_p_values[r, c])
-    
-    if (!is.nan(t_tests_p_values[r, c]) && t_tests_p_values[r, c] <= 0.05) {
-      #print("Lower than 0.05")
-      print(paste0("Construct ID: ", t_tests_p_values$construct_ID[r]))
-      #print(paste0("Assigned level: ", names(t_tests_p_values)[c]))
-      level_prediction[t_tests_p_values$construct_ID[r], "first_sign_p_pred"] <- str_split_i(names(t_tests_p_values)[c], "_", 2)
+    if(!is.na(t_tests_relevant_p_vals[r, c])){
+      print(t_tests_relevant_p_vals[r, c])
+      t_level_predictions$first_sign_p_pred[r] <- str_split_i(colnames(t_tests_relevant_p_vals)[c], "_", 2)
       break
     }
+      
   }
 }
 
-# find lowest p-value
-for (r in 1:nrow(t_tests_p_values)){
 
-    #print(paste0("Row ", r, " Column ", c))
-    
-    if (any(rowSums(t_tests_p_values[r, 2:6] <= 0.05, na.rm = TRUE))){
-      column_name <- names(t_tests_p_values)[apply(t_tests_p_values[r,], 1, which.min)]
+
+# find lowest p-value
+for (r in 1:nrow(t_tests_relevant_p_vals)){
+    if (any(!is.na(t_tests_relevant_p_vals[r, 2:6 ]))){
+      # get the name of the column with the minimum p-val
+      column_name <- names(t_tests_relevant_p_vals)[apply(t_tests_relevant_p_vals[r, 1:6], 1, which.min)]
       predicted_level <- str_split_i(column_name, "_", 2)
-      #print(paste("Predicted level:", predicted_level))
-      print(paste("Min p-val:", apply(t_tests_p_values[r,], 1, min)))
-      print(paste("Construct ID:", t_tests_p_values$construct_ID[r]))
-      #print(paste("EGP level:", t_tests_p_values[r, "EGP_level"]))
-      level_prediction[t_tests_p_values$construct_ID[r], "min_p_pred"] <- predicted_level
+      print(paste("Predicting:", predicted_level))
+      
+      print(paste("Construct ID:", t_tests_relevant_p_vals$construct_ID[r]))
+      
+      t_level_predictions[r, "min_p_pred"] <- predicted_level
     }
   
 }
 
-# Answer question: is there a significant different at the level the EGP claims that the construct is criterial for?
 
 
